c0dfa836668b295fb54c09df744ca9b9cf9117ea
umere126 <mumerpanni@gmail.com>
2025-10-16 02:34:04 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// You can add functionality later.
+// For now, just show a message when the
+button is clicked.
+document.querySelector("button").addEventListener("click",()=>{
+    alert("Shirt added to cart");
+});
